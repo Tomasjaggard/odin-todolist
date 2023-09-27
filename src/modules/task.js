e01aa6b520c4a1392d63ef2f01c826feda1063d5
Tomas <tomas@jaggard.net>
2023-09-27 11:34:32 +0100

    Refactor getDateFormatted

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,8 +1,8 @@
 export default class Task{
-    constructor(title, description, dueDate = 'None', priority = 'low'){
+    constructor(title, description, dueDate = null, priority = 'low'){
         this.title = title
         this.description = description
-        this.dueDate = dueDate
+        this.dueDate = dueDate instanceof Date ? dueDate : null;
         this.priority = priority
     }
 
@@ -23,10 +23,13 @@ export default class Task{
     }
 
     getDateFormatted() {
-        const dateSplit = this.dueDate.split('-')
-        const day = dateSplit[2]
-        const month = dateSplit[1]
-        const year = dateSplit[0]
-        return `${day}/${month}/${year}`
+        const day = this.dueDate.getDate();
+        const month = this.dueDate.getMonth() + 1;
+        const year = this.dueDate.getFullYear();
+
+        const formattedDay = day.toString().padStart(2, '0');
+        const formattedMonth = month.toString().padStart(2, '0');
+        const formattedDate = `${formattedDay}/${formattedMonth}/${year}`;
+        return formattedDate
     }
 }
